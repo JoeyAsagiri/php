326a50a4f1b8f619296fb41a4ef7300e93afe54c
Joey Smulders <Joey Smulders>
2016-03-08 09:44:24 +0100

    made an index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo '<a href="/PHP/kerstboom/main.php">Kerstboom</a>';
+echo '<br>';
+echo '<a href="/PHP/forwhile/main.php">forwhile</a>';
+echo '<br>';
+echo '<a href="/PHP/busreis/main.php">busreis</a>';
+echo '<br>';
+echo '<a href="/PHP/zwemclub/main.php">Zwemclub</a>';
+?>
